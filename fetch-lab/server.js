57667505b4ccb19ac55d418c529fc8f76a9080d0
Sushmita15 <sushmitamandal100@gmail.com>
2026-04-07 11:16:15 -0400

    Part A - B.3 done

---
+++ fetch-lab/server.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+// Serve static files from the 'public' folder
+app.use(express.static("public"));
+
+// Parse JSON request bodies (needed for POST)
+app.use(express.json());
+
+// ---- Your endpoints go below this line ----
+app.get("/hello", (req, res) => {
+  res.type("text").send("Hello from the server!");
+});
+
+//B.2
+app.get("/api/time", (req, res) => {
+  res.json({
+    currentTime: new Date().toISOString(),
+    message: "Current server time",
+  });
+});
+
+//B.3
+app.get("/api/greet/:name", (req, res) => {
+  const name = req.params.name;
+  res.json({
+    greeting: "Hello, ${name}! Welcome to the API.",
+  });
+});
+
+//B.4
+
+
+// ---- Your endpoints go above this line ----
+
+const PORT = process.env.PORT || 3000; //using port 3000 because 8080 is being used for discuit
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
